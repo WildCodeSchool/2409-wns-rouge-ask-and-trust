9441146a7a81466168c9497773dce92a1def6650
YohanGH <regnier.yohan@proton.me>
2025-02-14 15:02:24 +0100

    Add auth service to backend use template (tgc)

---
+++ app/backend/src/middlewares/error-handler.ts
@@ -0,0 +1,32 @@
+// Interface for Apperror structure
+interface IAppError {
+	statusCode: number
+	errorType?: string
+	additionalInfo?: string
+	isOperational?: boolean
+}
+
+// AppError class with proper TypeScript types
+export class AppError extends Error implements IAppError {
+	statusCode: number
+	errorType?: string
+	additionalInfo?: string
+	isOperational: boolean
+
+	constructor(
+		message: string,
+		statusCode: number = 500,
+		errorType?: string,
+		additionalInfo?: string,
+		isOperational: boolean = true
+	) {
+		super(message)
+		Object.setPrototypeOf(this, new.target.prototype)
+
+		this.statusCode = statusCode
+		this.errorType = errorType
+		this.additionalInfo = additionalInfo
+		this.isOperational = isOperational
+		Error.captureStackTrace(this)
+	}
+}
---
+++ app/backend/src/types/types.ts
@@ -0,0 +1,7 @@
+import { User } from "../database/entities/user";
+import Cookies from "cookies";
+
+export type Context = {
+    cookies: Cookies;
+    user: User | null | undefined;
+  };
