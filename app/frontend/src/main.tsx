8ccdc5c6d1286f4461632dafa47b1f717380f4e0
YohanGH <regnier.yohan@proton.me>
2025-02-14 10:07:32 +0100

    Add template (tgc) front-end in project

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import Router from "@/config/router.tsx";
+import "@/styles/global.css";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "@/config/client.tsx";
+import { AuthProvider } from "@/contexts/AuthContextProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ApolloProvider client={client}>
+        <AuthProvider>
+          <Router />
+          <Toaster position="bottom-right" />
+        </AuthProvider>
+    </ApolloProvider>
+  </StrictMode>
+);
