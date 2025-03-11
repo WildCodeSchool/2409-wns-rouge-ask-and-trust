c40315572e124a47e728c6db58db2f7e400a52ec
ArthurVS05 <arthur.vincentsil@gmail.com>
2025-03-11 14:49:00 +0100

    feat: added font family and variables

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,19 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Toaster } from "react-hot-toast";
-import Router from "@/config/router.tsx";
-import "@/styles/global.css";
-import { ApolloProvider } from "@apollo/client";
-import { client } from "@/config/client.tsx";
-import { AuthProvider } from "@/contexts/AuthContextProvider.tsx";
+import { client } from "@/config/client.tsx"
+import Router from "@/config/router.tsx"
+import { AuthProvider } from "@/contexts/AuthContextProvider.tsx"
+import "@/styles/index.css"
+import { ApolloProvider } from "@apollo/client"
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { Toaster } from "react-hot-toast"
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ApolloProvider client={client}>
-        <AuthProvider>
-          <Router />
-          <Toaster position="bottom-right" />
-        </AuthProvider>
-    </ApolloProvider>
-  </StrictMode>
+	<StrictMode>
+		<ApolloProvider client={client}>
+			<AuthProvider>
+				<Router />
+				<Toaster position="bottom-right" />
+			</AuthProvider>
+		</ApolloProvider>
+	</StrictMode>
 );
