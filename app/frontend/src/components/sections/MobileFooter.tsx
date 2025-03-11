02d18ecb20b62d8625fa4ac731832432e62aad10
Alexis <AlexDdevs@gmail.com>
2025-03-11 16:09:07 +0100

    Add type

--- app/frontend/src/components/sections/MobileFooter.tsx
+++ app/frontend/src/components/sections/MobileFooter.tsx
@@ -1,8 +1,14 @@
-import {House, SquarePlus, Bell, User} from "lucide-react"
-import { Link } from "react-router-dom"
+import { House, SquarePlus, Bell, User, LucideIcon } from "lucide-react";
+import { Link } from "react-router-dom";
+
+interface FooterItem {
+    Icon: LucideIcon;
+    content: string;
+    url: string;
+}
 
 export default function MobileFooter() {
-    const footerItems = [
+    const footerItems: FooterItem[] = [
         {
             Icon: House,
             content: "Accueil",
@@ -23,16 +29,16 @@ export default function MobileFooter() {
             content: "Profil",
             url: "/"
         },
-    ]
+    ];
 
-  return (
-    <footer className="flex justify-between px-5 py-2.5 bg-primary-600 fixed bottom-0 min-w-[375px] w-full sm:justify-around">
-        {footerItems.map(({Icon, content, url}) => (
-            <Link key={content} to={url} className="flex flex-col items-center gap-1 group">
-                <Icon className="transition-transform duration-200 ease-in-out group-hover:scale-110 text-button-primary-fg h-5 w-5" />
-                <span className="text-button-primary-fg text-xs">{content}</span>
-            </Link>
-        ))}
-    </footer>
-  )
+    return (
+        <footer className="flex justify-between px-5 py-2.5 bg-primary-600 fixed bottom-0 min-w-[375px] w-full sm:justify-around">
+            {footerItems.map(({ Icon, content, url }) => (
+                <Link key={content} to={url} className="flex flex-col items-center gap-1 group">
+                    <Icon className="transition-transform duration-200 ease-in-out group-hover:scale-110 text-button-primary-fg h-5 w-5" />
+                    <span className="text-button-primary-fg text-xs">{content}</span>
+                </Link>
+            ))}
+        </footer>
+    );
 }
