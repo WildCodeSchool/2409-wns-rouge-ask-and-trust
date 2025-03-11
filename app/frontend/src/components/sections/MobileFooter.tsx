75c0d19e4c43e7978f971985a353ec54a94d42df
Alexis <AlexDdevs@gmail.com>
2025-03-11 15:58:54 +0100

    Create a component for mobile footer

---
+++ app/frontend/src/components/sections/MobileFooter.tsx
@@ -0,0 +1,38 @@
+import {House, SquarePlus, Bell, User} from "lucide-react"
+import { Link } from "react-router-dom"
+
+export default function MobileFooter() {
+    const footerItems = [
+        {
+            Icon: House,
+            content: "Accueil",
+            url: "/"
+        },
+        {
+            Icon: SquarePlus,
+            content: "Créer",
+            url: "/"
+        },
+        {
+            Icon: Bell,
+            content: "Notifications",
+            url: "/"
+        },
+        {
+            Icon: User,
+            content: "Profil",
+            url: "/"
+        },
+    ]
+
+  return (
+    <footer className="flex justify-between px-5 py-2.5 bg-primary-600 fixed bottom-0 min-w-[375px] w-full sm:justify-around">
+        {footerItems.map(({Icon, content, url}) => (
+            <Link key={content} to={url} className="flex flex-col items-center gap-1 group">
+                <Icon className="transition-transform duration-200 ease-in-out group-hover:scale-110 text-button-primary-fg h-5 w-5" />
+                <span className="text-button-primary-fg text-xs">{content}</span>
+            </Link>
+        ))}
+    </footer>
+  )
+}
