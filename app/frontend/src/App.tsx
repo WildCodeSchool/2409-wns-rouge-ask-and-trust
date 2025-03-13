12da46f7de006f5074433376dbef454b71f80eb9
corenthin95 <corenthin.flamand@gmail.com>
2025-03-13 15:58:05 +0100

    added Information + new svg img + new class index.css

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import Navigation from "@/components/sections/Navigation"
 import Footer from "@/components/sections/Footer"
 import Hero from "@/components/sections/landing/Hero.tsx"
+import Information from "@/components/sections/landing/Information.tsx"
 
 function App() {
 	return (
 		<>
 			<Navigation />
 			<Hero />
+			<Information />
 			<Footer />
 		</>
 	)
---
+++ app/frontend/src/components/sections/landing/Information.tsx
@@ -0,0 +1,53 @@
+const Information = () => {
+	return (
+		<section
+			className="relative flex items-center justify-center overflow-hidden py-12 lg:py-20"
+			role="information"
+			aria-label="Deuxieme section"
+		>
+			<div className="py-12 lg:py-30" aria-hidden="true">
+				<svg
+					className="absolute top-0 left-0 z-0 h-[30rem] w-auto lg:h-[40rem] xl:h-auto"
+					viewBox="100 0 1920 791"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						fill="#F1F2FF"
+						d="M745.145 57.0757C459.14 85.6661 4.05875 33.423 -187.731 3.72766L-309 791C-125.47 696.942 1372.14 725.221 2098 751.118L2080.15 57.0757C1934.21 -51.1742 1102.65 21.3377 745.145 57.0757Z"
+					/>
+				</svg>
+			</div>
+			<div className="relative">
+				<div className="flex flex-col items-center lg:flex-row">
+					<div className="relative">
+						<img
+							src="../../../public/assets/window-text.svg"
+							alt="img-window"
+						/>
+						<div className="text-on-img absolute items-center px-2">
+							<h4 className="pb-2 text-2xl font-bold">
+								Une enquête personnalisée et adaptée à vos
+								besoins
+							</h4>
+							<h5 className="font-regular pb-2 text-sm sm:text-lg lg:text-lg">
+								Une fois votre enquête terminée, recevez les
+								résultats et analyser les réponses.
+							</h5>
+							<h5 className="font-regular text-sm sm:text-lg lg:text-lg">
+								Choisissez vos questions et comment peuvent
+								répondre les participants.
+							</h5>
+						</div>
+					</div>
+					<img
+						src="../../../public/assets/window-group-icons.svg"
+						alt="img-window-content"
+					/>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default Information
