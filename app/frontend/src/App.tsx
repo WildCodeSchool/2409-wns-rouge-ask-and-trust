e2d1d9a7f921c56b2f376008bb44cdbc5fa10ae2
Alexis <AlexDdevs@gmail.com>
2025-03-13 16:37:00 +0100

    Call Header component

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,19 +1,19 @@
+import Header from "./components/sections/Header";
 import { Outlet } from "react-router-dom";
-import Navigation from "@/components/sections/Navigation";
 import Footer from "@/components/sections/Footer";
 
 function App() {
-  return (
-    <>
-      <Navigation />
-      <main>
-        <div>
-          <Outlet />
-        </div>
-      </main>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main>
+                <div>
+                    <Outlet />
+                </div>
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
