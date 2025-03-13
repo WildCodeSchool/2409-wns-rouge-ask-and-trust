1a78b57f21815d269b5f2521bbb1c28fbff6613c
ArthurVS05 <arthur.vincentsil@gmail.com>
2025-03-13 22:32:26 +0100

    feat: added routes : /register and /connexion

--- app/frontend/src/config/router.tsx
+++ app/frontend/src/config/router.tsx
@@ -1,49 +1,65 @@
-import { lazy, Suspense } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "@/App.tsx";
-import NotFound from "@/pages/NotFound";
-import Loader from "@/components/ui/Loader";
-import ErrorElement from "@/components/ui/ErrorElement";
-import ProtectedRoute from "@/components/hoc/ProtectedRoute";
+import App from "@/App.tsx"
+import ErrorElement from "@/components/ui/ErrorElement"
+import Loader from "@/components/ui/Loader"
+import NotFound from "@/pages/NotFound"
+import { lazy, Suspense } from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 // Using lazy loading for pages
-const Home = lazy(() => import("@/pages/Home"));
+const Home = lazy(() => import("@/pages/Home"))
 const Auth = lazy(() => import("@/pages/Auth"))
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        errorElement: <ErrorElement />,
-        children: [
-            {
-                path: "/",
-                element: (
-                    <Suspense fallback={<Loader />}>
-                        <Home />
-                    </Suspense>
-                ),
-            },
-            {
-                path: "auth",
-                element: (
-                    //<ProtectedRoute> # example the use protected route here
-                        <Suspense fallback={<Loader />}>
-                            <Auth />
-                        </Suspense>
-                    //</ProtectedRoute>
-                )
-            },
-        ],
-    },
-    {
-        path: "*",
-        element: <NotFound />,
-    },
-]);
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <ErrorElement />,
+		children: [
+			{
+				path: "/",
+				element: (
+					<Suspense fallback={<Loader />}>
+						<Home />
+					</Suspense>
+				),
+			},
+			// {
+			// 	path: "auth",
+			// 	element: (
+			// 		//<ProtectedRoute> # example the use protected route here
+			// 		<Suspense fallback={<Loader />}>
+			// 			<Auth />
+			// 		</Suspense>
+			// 		//</ProtectedRoute>
+			// 	),
+			// },
+			// @TODO If user is connected, block access to Signin and Signup
+			{
+				path: "register",
+				element: (
+					<Suspense fallback={<Loader />}>
+						<Auth />
+					</Suspense>
+				),
+			},
+			{
+				path: "connexion",
+				element: (
+					<Suspense fallback={<Loader />}>
+						<Auth />
+					</Suspense>
+				),
+			},
+		],
+	},
+	{
+		path: "*",
+		element: <NotFound />,
+	},
+])
 
 const Router = () => {
-    return <RouterProvider router={router} />;
-};
+	return <RouterProvider router={router} />
+}
 
-export default Router;
+export default Router
