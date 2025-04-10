4ed82227f229880114f26a6a73d1ccd5b23cd4b5
ArthurVS05 <arthur.vincentsil@gmail.com>
2025-04-10 11:18:53 +0200

    feat: cleaned User entity and added typedoc

--- app/backend/src/database/entities/user.ts
+++ app/backend/src/database/entities/user.ts
@@ -1,20 +1,36 @@
 import { Field, ID, ObjectType } from "type-graphql"
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+import { Roles, UserRole } from "../../types/types"
 
-// This class represents a user entity in the database.
-// It defines the structure of a user, including its unique identifier (id),
-// the user's email address, and the user's hashed password.
+/**
+ * Represents a user entity in the database.
+ * @param name is the entity's name in database.
+ * This class defines the structure of the user entity in database:
+ * - `id`: unique identifier for the user.
+ * - `email`: user's email address (unique).
+ * - `hashedPassword`: the user's password stored in a hashed format (not exposed via GraphQL).
+ * - `firstname`: the user's first name.
+ * - `lastname`: the user's last name.
+ * - `role`: the user's role (defaults to `Roles.User`).
+ * - `createdAt`: timestamp of when the user was created.
+ * - `updatedAt`: timestamp of when the user was last updated.
+ *
+ * The class also leverages the following decorators:
+ * - `@Column()`: Tells TypeORM to store the property in the database.
+ * - `@Field()`: Exposes the property in the GraphQL schema (via type-graphql).
+ *
+ * For `createdAt` and `updatedAt`, the timestamps are automatically set by the database.
+ */
 @ObjectType()
-@Entity({ name: "user" }) // The name of the table in the database
+@Entity({ name: "user" })
 export class User extends BaseEntity {
 	@Field(() => ID)
 	@PrimaryGeneratedColumn()
-	id!: number // Unique identifier for the user
+	id!: number 
 
 	@Field()
 	@Column({ length: 254, unique: true })
-	email!: string // User's email address
-	// standard RFC 5321 for email's length
+	email!: string // standard RFC 5321 for email's length
 
 	@Column({ length: 255 })
 	hashedPassword!: string // User's hashed password (not exposed via GraphQL)
@@ -28,17 +44,21 @@ export class User extends BaseEntity {
 	lastname!: string
 
 	@Field()
-	@Column({ enum: ["user", "admin"], default: "user" })
-	role!: "user" | "admin" | "moderator" // User's role
+	@Column({
+		type: "enum",
+		enum: Roles,
+		default: Roles.User,
+	})
+	role!: UserRole
 
 	@Field()
 	@Column({ default: () => "CURRENT_TIMESTAMP" })
-	createdAt!: Date // The date and time when the user was created
+	createdAt!: Date 
 
 	@Field()
 	@Column({
 		default: () => "CURRENT_TIMESTAMP",
 		onUpdate: "CURRENT_TIMESTAMP",
 	})
-	updatedAt!: Date // The date and time when the user was last updated
+	updatedAt!: Date 
 }
