5587d744b4caf4188a28239e9a4e0a21ffc87894
corenthin95 <corenthin.flamand@gmail.com>
2025-04-10 10:33:57 +0200

    new package + check mark + update on app

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,19 +1,33 @@
-import Header from "@/components/sections/header/Header";
-import { Outlet } from "react-router-dom";
-import Footer from "@/components/sections/footer/Footer";
+import Header from "@/components/sections/header/Header"
+import Footer from "@/components/sections/footer/Footer"
+import Hero from "@/components/sections/landing/Hero"
+import Information from "@/components/sections/landing/Information"
+import Package from "@/components/sections/landing/Package"
+import Poll from "@/components/sections/landing/Poll"
+import { Button } from "@/components/ui/Button.tsx"
 
 function App() {
-    return (
-        <>
-            <Header />
-            <main>
-                <div>
-                    <Outlet />
-                </div>
-            </main>
-            <Footer />
-        </>
-    );
+	return (
+		<>
+			<Header />
+			<Hero />
+			<Information />
+			<Poll />
+			<Package />
+			<div className="flex flex-row justify-center px-5 pt-4 pb-30 lg:pt-10 lg:pb-60">
+				<Button
+					variant="secondary"
+					fullWidth
+					role="link"
+					ariaLabel="S'inscrire"
+					className="mb-2.5 flex h-10 items-center justify-center rounded-md text-base lg:m-0 lg:w-48"
+				>
+					S'inscrire
+				</Button>
+			</div>
+			<Footer />
+		</>
+	)
 }
 
-export default App;
+export default App
