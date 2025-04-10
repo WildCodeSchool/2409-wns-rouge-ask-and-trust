fe2a60ebc4aa471bf1c6fb91067de22615ad132a
Alexis <AlexDdevs@gmail.com>
2025-04-10 10:33:10 +0200

    Create a component to display Pagination

---
+++ app/frontend/src/components/ui/Pagination.tsx
@@ -0,0 +1,38 @@
+import {
+	PaginationContainer,
+	PaginationContent,
+	PaginationEllipsis,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+} from "./PaginationContainer"
+
+export default function Pagination() {
+	return (
+		<PaginationContainer>
+			<PaginationContent>
+				<PaginationItem>
+					<PaginationPrevious href="#" />
+				</PaginationItem>
+				<PaginationItem>
+					<PaginationLink href="#" isActive={true}>
+						1
+					</PaginationLink>
+				</PaginationItem>
+				<PaginationItem>
+					<PaginationLink href="#">2</PaginationLink>
+				</PaginationItem>
+				<PaginationItem>
+					<PaginationLink href="#">3</PaginationLink>
+				</PaginationItem>
+				<PaginationItem>
+					<PaginationEllipsis />
+				</PaginationItem>
+				<PaginationItem>
+					<PaginationNext href="#" />
+				</PaginationItem>
+			</PaginationContent>
+		</PaginationContainer>
+	)
+}
