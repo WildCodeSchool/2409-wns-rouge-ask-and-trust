b7046fbb5f43f0c71823753ab34ecf6384ee2b38
ArthurVS05 <arthur.vincentsil@gmail.com>
2025-04-10 10:18:40 +0200

    wip: updating User entity and input User in Backend

--- app/backend/src/database/entities/user.ts
+++ app/backend/src/database/entities/user.ts
@@ -1,5 +1,5 @@
+import { Field, ID, ObjectType } from "type-graphql"
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm"
-import { ObjectType, Field, ID } from "type-graphql"
 
 // This class represents a user entity in the database.
 // It defines the structure of a user, including its unique identifier (id),
@@ -12,15 +12,24 @@ export class User extends BaseEntity {
 	id!: number // Unique identifier for the user
 
 	@Field()
-	@Column({ length: 255, unique: true })
+	@Column({ length: 254, unique: true })
 	email!: string // User's email address
+	// standard RFC 5321 for email's length
 
 	@Column({ length: 255 })
-	password!: string // User's hashed password (not exposed via GraphQL)
+	hashedPassword!: string // User's hashed password (not exposed via GraphQL)
 
-	@Column({ enum: ["user", "admin"], default: "user" })
 	@Field()
-	role!: string // "user" | "admin"
+	@Column({ length: 100 })
+	firstname!: string
+
+	@Field()
+	@Column({ length: 100 })
+	lastname!: string
+
+	@Field()
+	@Column({ enum: ["user", "admin"], default: "user" })
+	role!: "user" | "admin" | "moderator" // User's role
 
 	@Field()
 	@Column({ default: () => "CURRENT_TIMESTAMP" })
