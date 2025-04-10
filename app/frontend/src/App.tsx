6c2a9ea6bb35c573804e1929e6b75388a25de643
Alexis <AlexDdevs@gmail.com>
2025-04-10 10:34:41 +0200

    Edit App to display mobile Footer and HeaderSurveys

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,18 +1,37 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import Footer from "./components/sections/footer/Footer"
 import Header from "./components/sections/header/Header"
+import HeaderSurveys from "./components/sections/surveys/Header"
+import { useEffect, useState } from "react"
+import FooterMobile from "./components/sections/footer/FooterMobile"
 
 function App() {
+	const location = useLocation()
+	const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth < 768)
+		}
+
+		window.addEventListener("resize", handleResize)
+		return () => window.removeEventListener("resize", handleResize)
+	}, [])
+
 	return (
 		<>
-			<Header />
+			{location.pathname === "/surveys" ? <HeaderSurveys /> : <Header />}
 			{/* @TODO calc height : fill screen minus Header height. On mobile : minus Header height and Navbar height. */}
-			<main className="bg-bg">
-				<div className="h-[100%]">
+			<main className="bg-bg mb-20">
+				<div className="h-full">
 					<Outlet />
 				</div>
 			</main>
-			<Footer />
+			{location.pathname === "/surveys" && isMobile ? (
+				<FooterMobile />
+			) : (
+				<Footer />
+			)}
 		</>
 	)
 }
