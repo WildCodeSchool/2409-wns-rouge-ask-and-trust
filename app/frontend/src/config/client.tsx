9ed9aafb3dc650f12cee2f9dfa2b30d7430acbbf
YohanGH <regnier.yohan@proton.me>
2025-04-17 11:30:56 +0200

    add header for stripe

--- app/frontend/src/config/client.tsx
+++ app/frontend/src/config/client.tsx
@@ -4,5 +4,16 @@ import { VITE_GRAPHQL_ENDPOINT } from "@/config/config"
 export const client = new ApolloClient({
 	uri: VITE_GRAPHQL_ENDPOINT,
 	cache: new InMemoryCache(),
-	credentials: "same-origin",
+	credentials: "include",
+	headers: {
+		"Access-Control-Allow-Credentials": "true",
+	},
+	defaultOptions: {
+		watchQuery: {
+			fetchPolicy: "network-only",
+		},
+		query: {
+			fetchPolicy: "network-only",
+		},
+	},
 })
