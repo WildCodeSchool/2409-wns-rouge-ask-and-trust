2e4ac2acb2e5bc7b97ffcdd0951c0fa0ba4742cf
ArthurVS05 <arthur.vincentsil@gmail.com>
2025-04-18 11:12:02 +0200

    feat: script to create an admin user after database's initialisation

---
+++ app/backend/src/scripts/create-admin.ts
@@ -0,0 +1,57 @@
+import { User } from "../database/entities/user"
+import { register } from "../services/auth-service"
+import { Roles, UserRole } from "../types/types"
+
+async function createAdmin() {
+	const email = process.env.ADMIN_EMAIL
+	const password = process.env.ADMIN_PASSWORD
+	const firstname = process.env.ADMIN_FIRSTNAME
+	const lastname = process.env.ADMIN_LASTNAME
+		? (process.env.ADMIN_ROLE as UserRole)
+		: Roles.User
+
+	// Verify environment variables
+	if (!email) {
+		console.error("❌ ADMIN_EMAIL is not defined.")
+		return
+	}
+	if (!password) {
+		console.error("❌ ADMIN_PASSWORD is not defined.")
+		return
+	}
+	if (!firstname) {
+		console.error("❌ ADMIN_FIRSTNAME is not defined.")
+		return
+	}
+	if (!lastname) {
+		console.error("❌ ADMIN_LASTNAME is not defined.")
+		return
+	}
+
+	try {
+		// Verify if an admin exists in database
+		const isAlreadyUserInDatabase = await User.find({
+			where: { email },
+			select: ["id", "email"], // Don't want to get hashedPassword
+		})
+
+		if (isAlreadyUserInDatabase) {
+			return
+		}
+
+		// Create an Admin user if doesn't exist in database
+		const user = await register(
+			email,
+			password,
+			firstname,
+			lastname,
+			Roles.Admin
+		)
+
+		console.log(`✅ Admin user created: ${user.email}`)
+	} catch (error) {
+		console.error("❌ Failed to create admin user:", error)
+	}
+}
+
+export { createAdmin }
