6afdef721288c8363fae06524fb0dbf8fbf441b8
Alexis <AlexDdevs@gmail.com>
2025-04-18 17:18:23 +0200

    Create types for survey

--- app/backend/src/types/types.ts
+++ app/backend/src/types/types.ts
@@ -13,3 +13,47 @@ export const Roles = {
 } as const
 
 export type UserRole = (typeof Roles)[keyof typeof Roles]
+
+export type UserType = {
+	id: number
+	firstname: string
+	lastname: string
+	email: string
+	hashedPassword: string
+	role: UserRole
+	surveys: SurveyType[]
+	createdAt: Date
+	updatedAt: Date
+}
+
+export type SurveyType = {
+	id: number
+	name: string
+	description: string
+	category: SurveyCategoryType
+	questions: SurveyQuestionsType[]
+	public: boolean
+	createdAt: Date
+	updatedAt: Date
+}
+
+export type SurveyCategoryType = {
+	id: number
+	name: string
+	createdAt: Date
+	updatedAt: Date
+	surveys: SurveyType[]
+}
+
+export type SurveyQuestionsType = {
+	id: number
+	title: string
+	type: TypeOfQuestion
+	answers: string[]
+}
+
+export enum TypeOfQuestion {
+	TEXT = "text",
+	MULTIPLE_CHOICE = "multiple_choice",
+	BOOLEAN = "boolean",
+}
