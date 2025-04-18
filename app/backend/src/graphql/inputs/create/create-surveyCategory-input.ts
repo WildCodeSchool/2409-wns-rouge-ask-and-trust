5e12ae07f542beec76f45deeed0db25fb3472e06
Alexis <AlexDdevs@gmail.com>
2025-04-18 17:15:02 +0200

    Create inputs for survey  category

---
+++ app/backend/src/graphql/inputs/create/create-surveyCategory-input.ts
@@ -0,0 +1,22 @@
+import { Length } from "class-validator"
+import { Field, InputType } from "type-graphql"
+
+/**
+ * Represents input data for creating a new survey category.
+ * This class is used to validate the category data before storing it in the database.
+ *
+ * @description
+ * - `name`: the name of the survey category, must be between 5 and 100 characters.
+ *
+ * The class uses the following decorators:
+ * - `@Field()`: Exposes the property in the GraphQL schema (via type-graphql).
+ * - `@Length()`: Ensures the string length is within the specified bounds.
+ */
+@InputType()
+export class CreateCategorySurveyInput {
+	@Field()
+	@Length(5, 100, {
+		message: "Category name must be between 5 and 100 characters.",
+	})
+	name!: string
+}
