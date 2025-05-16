1ecf4c0848e992856229b167e4cd5d18d8c3b609
ArthurVS05 <arthur.vincentsil@gmail.com>
2025-05-16 15:01:16 +0200

    feat: added ui Switch and TypeSwitch for Question

---
+++ app/frontend/src/components/ui/Switch.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils"
+import * as SwitchPrimitives from "@radix-ui/react-switch"
+import React, { forwardRef } from "react"
+
+interface SwitchProps
+	extends React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root> {
+	checked: boolean
+	onCheckedChange: (checked: boolean) => void
+	id: string
+}
+
+const Switch = forwardRef<
+	React.ComponentRef<typeof SwitchPrimitives.Root>,
+	SwitchProps
+>(({ className, checked, onCheckedChange, id, ...props }, ref) => (
+	<SwitchPrimitives.Root
+		className={cn(
+			// Background switch
+			"peer inline-flex h-[24px] w-[44px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors",
+			"focus-visible:ring-focus focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none",
+			"disabled:cursor-not-allowed disabled:opacity-50",
+			// Change background color if activated
+			"data-[state=checked]:bg-active",
+			"data-[state=unchecked]:bg-disabled",
+			className
+		)}
+		id={id}
+		checked={checked}
+		onCheckedChange={onCheckedChange}
+		{...props}
+		ref={ref}
+	>
+		<SwitchPrimitives.Thumb
+			className={cn(
+				// Circle switch
+				"pointer-events-none block h-5 w-5 rounded-full shadow-lg ring-0 transition-transform",
+				"bg-white",
+				"data-[state=checked]:translate-x-5",
+				"data-[state=unchecked]:translate-x-0"
+			)}
+		/>
+	</SwitchPrimitives.Root>
+))
+Switch.displayName = SwitchPrimitives.Root.displayName
+
+export { Switch }
