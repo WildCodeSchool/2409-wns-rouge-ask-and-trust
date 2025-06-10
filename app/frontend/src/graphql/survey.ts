b280c9115f4e1daadfb37fce4e7ee8b2ed1ed21d
YohanGH <regnier.yohan@proton.me>
2025-06-10 18:20:35 +0200

    graphql: Create survey and category graphql queries

---
+++ app/frontend/src/graphql/survey.ts
@@ -0,0 +1,56 @@
+import { gql } from "@apollo/client"
+
+export const GET_SURVEYS = gql`
+	query Surveys {
+		surveys {
+			id
+			title
+			description
+			category {
+				id
+				name
+			}
+			user {
+				id
+				email
+			}
+			# Ajoute ici les autres champs nécessaires pour l'affichage
+		}
+	}
+`
+
+export const CREATE_SURVEY = gql`
+	mutation CreateSurvey($data: CreateSurveyInput!) {
+		createSurvey(data: $data) {
+			id
+			title
+			description
+			category {
+				id
+				name
+			}
+			user {
+				id
+				email
+			}
+		}
+	}
+`
+
+export const UPDATE_SURVEY = gql`
+	mutation UpdateSurvey($id: ID!, $data: UpdateSurveyInput!) {
+		updateSurvey(id: $id, data: $data) {
+			id
+			title
+			description
+			category {
+				id
+				name
+			}
+			user {
+				id
+				email
+			}
+		}
+	}
+`
