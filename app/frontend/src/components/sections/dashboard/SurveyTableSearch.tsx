59048e1666317fdef51fde4426162d02a70eb417
Alexis <AlexDdevs@gmail.com>
2025-07-12 17:38:50 +0200

    refactor: remove react hook form and improve research

--- app/frontend/src/components/sections/dashboard/SurveyTableSearch.tsx
+++ app/frontend/src/components/sections/dashboard/SurveyTableSearch.tsx
@@ -1,31 +1,22 @@
 import { Search } from "lucide-react"
-import { useForm } from "react-hook-form"
 import { Label } from "@/components/ui/Label"
 import { Input } from "@/components/ui/Input"
 import { Button } from "@/components/ui/Button"
 
-type SearchFormData = {
-	search: string
-}
-
 type SurveyTableSearchProps = {
-	onSearch: (query: string) => void
+	value: string
+	setValue: (query: string) => void
 }
 
 export default function SurveyTableSearch({
-	onSearch,
+	value,
+	setValue,
 }: SurveyTableSearchProps) {
-	const { register, handleSubmit } = useForm<SearchFormData>({
-		defaultValues: { search: "" },
-	})
-
-	const onFormSubmit = (data: SearchFormData) => {
-		onSearch(data.search)
-	}
-
 	return (
 		<form
-			onSubmit={handleSubmit(onFormSubmit)}
+			onSubmit={(e: React.FormEvent<HTMLFormElement>) =>
+				e.preventDefault()
+			}
 			className="relative flex max-w-3xs flex-1 items-center justify-center max-sm:order-1"
 		>
 			<Button
@@ -43,12 +34,9 @@ export default function SurveyTableSearch({
 				type="search"
 				id="search"
 				placeholder="Rechercher une enquête"
+				value={value}
 				errorMessage=""
-				{...register("search")}
-				onChange={e => {
-					const value = e.target.value
-					onSearch(value)
-				}}
+				onChange={e => setValue(e.target.value)}
 				className="border-button-line-border text-input-fg h-10 bg-white pl-14 text-sm"
 			/>
 		</form>
