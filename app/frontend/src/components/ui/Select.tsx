0da84a1d69cd981fbd8bb8111e2e4e925bf1035b
corenthin95 <corenthin.flamand@gmail.com>
2025-07-16 17:04:43 +0200

    fix(profile): rollback rounded button size

--- app/frontend/src/components/ui/Select.tsx
+++ app/frontend/src/components/ui/Select.tsx
@@ -30,7 +30,7 @@ const SelectTrigger = forwardRef<
 	<SelectTriggerBase
 		ref={ref}
 		className={cn(
-			"border-black-100 ring-offset-background data-[placeholder]:text-muted-foreground focus:ring-focus group flex h-10 w-full cursor-pointer items-center justify-between rounded-lg border bg-transparent px-3 py-2 text-sm whitespace-nowrap focus:ring-1 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+			"border-black-100 ring-offset-background data-[placeholder]:text-muted-foreground focus:ring-focus group flex h-10 w-full cursor-pointer items-center justify-between rounded-md border bg-transparent px-3 py-2 text-sm whitespace-nowrap focus:ring-1 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
 			className
 		)}
 		{...props}
