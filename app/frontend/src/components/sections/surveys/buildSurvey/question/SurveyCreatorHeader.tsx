c13eb17527553c599b1ff34ab6bcfc735e67064f
ArthurVS05 <arthur.vincentsil@gmail.com>
2025-09-23 21:10:44 +0200

    fix: padding in survey creator header

--- app/frontend/src/components/sections/surveys/buildSurvey/question/SurveyCreatorHeader.tsx
+++ app/frontend/src/components/sections/surveys/buildSurvey/question/SurveyCreatorHeader.tsx
@@ -36,8 +36,8 @@ export function SurveyCreatorHeader({
 				)}
 				<div
 					className={cn(
-						"shadow-default border-black-50 flex h-fit max-h-[calc(100vh_-_var(--header-height)_-_var(--footer-height)_-_10vh)] flex-col rounded-xl border bg-white p-4 transition-all duration-200 ease-in-out md:max-h-fit",
-						isExpanded && "gap-4",
+						"shadow-default border-black-50 flex h-fit max-h-[calc(100vh_-_var(--header-height)_-_var(--footer-height)_-_10vh)] flex-col rounded-xl border bg-white p-4 pb-6 transition-all duration-200 ease-in-out md:max-h-fit",
+						isExpanded && "gap-4 pb-4",
 						!isExpanded && isTransitionEnded && "gap-0"
 					)}
 				>
