05d10a5b3966b714418321194e704467815e7323
ArthurVS05 <arthur.vincentsil@gmail.com>
2025-09-24 13:13:20 +0200

    fix: infinite re-render in app.tsx, create RenderHeader and RenderFooter components

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -6,47 +6,67 @@ import Footer from "./components/sections/footer/Footer"
 import Header from "./components/sections/header/Header"
 import { useResponsivity } from "./hooks/useResponsivity"
 
+const RenderHeader = ({
+	removeHeader,
+	pathname,
+}: {
+	removeHeader: boolean
+	pathname: string
+}) => {
+	if (removeHeader) {
+		return null
+	}
+
+	switch (pathname) {
+		case "/":
+			return <Header />
+		case "/surveys":
+			return <HeaderSurveys isInSurveys />
+		default:
+			return <HeaderSurveys />
+	}
+}
+const RenderFooter = ({
+	isMobile,
+	bgBlue,
+	pathname,
+}: {
+	isMobile: boolean
+	bgBlue: boolean
+	pathname: string
+}) => {
+	if (pathname !== "/" && isMobile) {
+		return <FooterMobile bgBlue={bgBlue} />
+	}
+	return <Footer />
+}
+
 function App() {
 	const location = useLocation()
 	const { rootRef, isHorizontalCompact } = useResponsivity(Infinity, 768)
-	const removeHeaderFromSurveyCreator = Boolean(
+	const isMobileInSurveyCreatorPage = Boolean(
 		matchPath("/surveys/build/:id", location.pathname) &&
 			isHorizontalCompact
 	)
 
-	const RenderHeader = () => {
-		if (removeHeaderFromSurveyCreator) {
-			return null
-		}
-
-		switch (location.pathname) {
-			case "/":
-				return <Header />
-			case "/surveys":
-				return <HeaderSurveys isInSurveys />
-			default:
-				return <HeaderSurveys />
-		}
-	}
-
-	const RenderFooter = () => {
-		if (location.pathname !== "/" && isHorizontalCompact) {
-			return <FooterMobile bgBlue={removeHeaderFromSurveyCreator} />
-		}
-		return <Footer />
-	}
-
 	return (
 		<>
 			<Toaster richColors position="bottom-center" closeButton />
-			<RenderHeader />
+			<RenderHeader
+				removeHeader={isMobileInSurveyCreatorPage}
+				pathname={location.pathname}
+			/>
 			{/* @TODO calc height : fill screen minus Header height. On mobile : minus Header height and Navbar height. */}
 			<main className="bg-bg" ref={rootRef}>
 				<div className="h-full">
 					<Outlet />
 				</div>
 			</main>
-			<RenderFooter />
+			<RenderFooter
+				isMobile={isHorizontalCompact}
+				bgBlue={isMobileInSurveyCreatorPage}
+				pathname={location.pathname}
+			/>
 		</>
 	)
 }
