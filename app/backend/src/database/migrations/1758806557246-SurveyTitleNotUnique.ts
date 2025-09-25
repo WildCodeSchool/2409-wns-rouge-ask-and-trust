3310c0b95d4ac5735dd0e0508e2e82444b6e8f33
ArthurVS05 <arthur.vincentsil@gmail.com>
2025-09-25 15:34:01 +0200

    fix: migration name

--- app/backend/src/database/migrations/1758806557246-SurveyTitleNotUnique.ts
+++ app/backend/src/database/migrations/1758806557246-SurveyTitleNotUnique.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class Test1758806557246 implements MigrationInterface {
-	name = "Test1758806557246"
+export class SurveyTitleNotUnique1758806557246 implements MigrationInterface {
+	name = "SurveyTitleNotUnique1758806557246"
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query(
