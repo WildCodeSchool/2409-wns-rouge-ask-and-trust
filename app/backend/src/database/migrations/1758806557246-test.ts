77f54aa32972eaec0f976559c73ba4a0e42ed807
ArthurVS05 <arthur.vincentsil@gmail.com>
2025-09-25 15:29:57 +0200

    fix: generate migration to update schema - survey's title is not unique now

---
+++ app/backend/src/database/migrations/1758806557246-test.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class Test1758806557246 implements MigrationInterface {
+	name = "Test1758806557246"
+
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE "survey" DROP CONSTRAINT "UQ_af2eef92c9425e355b20bd941fc"`
+		)
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE "survey" ADD CONSTRAINT "UQ_af2eef92c9425e355b20bd941fc" UNIQUE ("title")`
+		)
+	}
+}
