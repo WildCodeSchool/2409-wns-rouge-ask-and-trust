691e1a7c74968935eeb547b8e10856625716ce15
ArthurVS05 <arthur.vincentsil@gmail.com>
2025-10-19 19:29:30 +0200

    fix: vite js hot reload

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -2,6 +2,10 @@ import react from "@vitejs/plugin-react"
 import path from "path"
 import { defineConfig } from "vitest/config"
 
+console.log(
+	'process.env.VITE_NODE_ENV === "development"',
+	process.env.VITE_NODE_ENV === "development"
+)
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
