8b285d54b3185f7adf8ba0b518666ae6a39faa5a
ArthurVS05 <arthur.vincentsil@gmail.com>
2025-10-19 19:31:51 +0200

    fix: clean log

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -2,10 +2,6 @@ import react from "@vitejs/plugin-react"
 import path from "path"
 import { defineConfig } from "vitest/config"
 
-console.log(
-	'process.env.VITE_NODE_ENV === "development"',
-	process.env.VITE_NODE_ENV === "development"
-)
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
