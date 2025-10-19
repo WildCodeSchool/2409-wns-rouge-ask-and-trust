d684433c5654c4bf2685590d561a3ff49a60e6ed
ArthurVS05 <arthur.vincentsil@gmail.com>
2025-10-19 16:45:36 +0200

    fix: clean datasource.ts

--- app/backend/src/database/config/datasource.ts
+++ app/backend/src/database/config/datasource.ts
@@ -2,17 +2,12 @@ import { DataSource } from "typeorm"
 import path from "path"
 import dotenv from "dotenv"
 
-// const isTest =
-// 	process.env.NODE_ENV === "testing" || process.env.NODE_ENV === "test"
-// const envFile = isTest
-// 	? path.resolve(process.cwd(), "test.env")
-// 	: path.resolve(process.cwd(), ".env")
-
-// dotenv.config({ path: envFile })
-const isTest =
+const IS_TEST =
 	process.env.NODE_ENV === "testing" || process.env.NODE_ENV === "test"
+const IS_CI = process.env.CI === "true"
+const IS_DEV = process.env.IS_DEV === "true"
 
-if (isTest && process.env.CI !== "true") {
+if (IS_TEST && !IS_CI) {
 	const envFile = path.resolve(process.cwd(), "test.env")
 	dotenv.config({ path: envFile })
 } else {
@@ -21,18 +16,21 @@ if (isTest && process.env.CI !== "true") {
 
 const dataSource = new DataSource({
 	type: "postgres",
-	host: process.env.DB_HOST, // db_test in ci, localhost when local testing
+	// In testing environment, create a separate database
+	host: process.env.DB_HOST,
 	port: parseInt(process.env.DB_PORT || "5432"),
 	username: process.env.POSTGRES_USER,
 	password: process.env.POSTGRES_PASSWORD,
 	database: process.env.POSTGRES_DB,
 	entities: [path.join(__dirname, "../entities/**/*.{ts,js}")],
-	synchronize: process.env.IS_DEV === "true" || isTest, // in prod environment, IS_DEV is false and Typeorm runs migrations scripts.
-	dropSchema: isTest,
+	// Synchronize schema only in dev or test environment
+	synchronize: IS_DEV || IS_TEST,
+	dropSchema: IS_TEST,
 	migrations: [path.join(__dirname, "../migrations/*.{ts,js}")],
 	migrationsTableName: "migrations",
-	logging: !isTest,
-	migrationsRun: !isTest && process.env.IS_DEV !== "true", // Run migrations only in production (IS_DEV=false)
+	// Run migrations only in production
+	migrationsRun: !IS_DEV && !IS_TEST,
+	logging: !IS_TEST,
 })
 
 export default dataSource
