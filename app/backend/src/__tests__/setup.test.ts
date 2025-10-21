0a5a11d309d9bf084f99b452b0d8b7613252be00
corenthin95 <corenthin.flamand@gmail.com>
2025-10-21 17:02:51 +0200

    test(test): added in setup file question tests

--- app/backend/src/__tests__/setup.test.ts
+++ app/backend/src/__tests__/setup.test.ts
@@ -16,6 +16,7 @@ import { User } from "../database/entities/user"
 import Cookies from "cookies"
 import { CustomAuthTest } from "./resolvers/CustomAuthTest"
 import { MySurveysResolverTest } from "./resolvers/MySurveysResolverTest"
+import { QuestionResolverTest } from "./resolvers/QuestionResolverTest"
 
 /**
  * index.spec.ts
@@ -179,6 +180,10 @@ describe("Surveys resolver", () => {
  */
 describe("My surveys resolver", () => {
 	MySurveysResolverTest(testArgs)
+})
+
+describe("Questions resolver", () => {
+	QuestionResolverTest(testArgs)
 })
 
 /**
